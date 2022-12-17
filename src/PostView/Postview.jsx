b8066ba24ea1e1890b2c6528dc5b850a09b850f1
Sayan-mitra1234 <119873741+Sayan-mitra1234@users.noreply.github.com>
2022-12-17 12:18:45 +0530

    Update Postview.jsx

--- src/PostView/Postview.jsx
+++ src/PostView/Postview.jsx
@@ -4,7 +4,7 @@ import Header from './Header';
 import Post from './Post';
 import Form from './Form';
  import axios from 'axios';
- const apiURL = 'https://instabackend-16xq.onrender.com';
+ const apiURL = 'https://instabackend-16xq.onrender.com/post';
 const Postview = () => {
     let [data, setdata] = useState([])
     let [postAdded, setPostAdded] = useState(0);
